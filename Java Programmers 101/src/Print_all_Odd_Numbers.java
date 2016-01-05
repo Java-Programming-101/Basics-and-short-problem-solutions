8f20864c7581641a3bc6b4c452b056adf0c95d87
Java Programming 101 <henrywright88404@gmail.com>
2016-01-06 12:18:33 +1300

    Added Print all odd numbers

---
+++ Java Programmers 101/src/Print_all_Odd_Numbers.java
@@ -0,0 +1,14 @@
+
+public class Print_all_Odd_Numbers {
+
+	public static void main(String[] args) {
+	 
+	for (int i = 1 ; i<100; i++){	
+		if (i % 2 !=0 ){ // check to see if the number is odd
+			System.out.println(i);
+			}
+		}
+	}
+}
+
+
