f3d69bc1ee3c6a53461d906526552c319674d389
Java Programming 101 <henrywright88404@gmail.com>
2016-01-28 12:57:57 +1300

    Added keywords and the main method

---
+++ Java Programmers 101/src/Add_numbers_from_file.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Add_numbers_from_file {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("C:\\Users\\Marc\\git\\Basics-and-short-problem-solutions\\Java Programmers 101\\src\\numbers_test.txt");
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			int total = 0;
+			String number;
+			while ((number = in.readLine()) != null) {
+				total = total + Integer.parseInt(number);
+			}
+			System.out.println(total);
+		} catch (Exception e) {
+			System.out.print("no file found");
+		}
+
+	}
+
+}
---
+++ Java Programmers 101/src/Keywords_and_the_main_method.java
@@ -0,0 +1,29 @@
+
+/*
+ * A keyword is one of 50 reserved words that have a predefined meaning in Java. 
+ * Because of this, programmers cannot use keywords as names for variables, methods, 
+ * classes, or as any other identifier.
+ * 
+ * In most development environments they will be highlighted (for example in eclipse 
+ * keywords are highlighted in purple. 
+ * 
+ * keywords are always lower case. 
+ */
+
+public class Keywords_and_the_main_method {
+
+	
+	/*
+	 * The method signature for the main() method contains three modifiers:
+	 * public indicates that the main() method can be called by any object. 
+	 * static indicates that the main() method is a class method. 
+	 * void indicates that the main() method has no return value.
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java programmers!");
+
+	}
+
+	
+}
